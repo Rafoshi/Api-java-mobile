99af941922a0e3d4c78fd1c8fb139e29a4fbb868
Rafoshi <rafa.ioshi@gmail.com>
2021-06-15 18:30:31 -0300

    Finished the sensors and geolocalization uses

---
+++ app/src/main/java/com/example/api/OxfordLocation.java
@@ -0,0 +1,39 @@
+package com.example.api;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class OxfordLocation extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oxford_location);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(51.7520228, -1.2580982);
+        mMap.setMinZoomPreference(16.0f);
+        mMap.setMaxZoomPreference(16.0f);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Oxford"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
